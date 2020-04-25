8cb034b80181c1d2e5dcccd7f7e8edd5f6619c62
simone-art <simoneperiodista3@gmail.com>
2020-04-25 12:15:54 -0300

    criado componente cabecalho

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CabecalhoComponent } from './cabecalho/cabecalho.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CabecalhoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
