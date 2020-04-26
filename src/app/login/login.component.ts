831db53027bfc3f19470a1d460b0a337ad5e1970
simone-art <simoneperiodista3@gmail.com>
2020-04-26 00:12:35 -0300

    Criada tela-login html e css

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,8 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   constructor() { }
-
   ngOnInit(): void {
+  }
+  enviarDados(){
+    console.log ('Babú perdeu o Big Brother');
   }
 
 }
