9b7ecc330b4404bc238c2c72c0675ef75233df6d
simone-art <simoneperiodista3@gmail.com>
2020-04-26 11:02:14 -0300

    Pegando o valor digitado no input usando keyup e função

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,11 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  InitialValue = 'Digite o seu usuário';
+
   constructor() { }
   ngOnInit(): void {
   }
   enviarDados(){
     console.log ('Babú perdeu o Big Brother');
+  }
+
+  digitouNome($event){
+    console.log($event);
+
   }
 
 }
